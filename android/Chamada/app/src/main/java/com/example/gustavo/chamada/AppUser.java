2cc776982e199938aa199dc6c6be0b4d19257331
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2017-05-13 17:22:43 -0300

    Implemented login confirmation... we will now refactor this since it can be usefull for other activities, including login

--- android/Chamada/app/src/main/java/com/example/gustavo/chamada/AppUser.java
+++ android/Chamada/app/src/main/java/com/example/gustavo/chamada/AppUser.java
@@ -1,5 +1,23 @@
 package com.example.gustavo.chamada;
 
+import android.app.Application;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.media.MediaBrowserCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
 /* This class is used to define a singleton containing the user of the application */
 class AppUser {
 
@@ -20,4 +38,5 @@ class AppUser {
     public static boolean hasLoggedInUser() {
         return currentUser != null;
     }
+
 }
--- android/Chamada/app/src/main/java/com/example/gustavo/chamada/RegisterChangeActivity.java
+++ android/Chamada/app/src/main/java/com/example/gustavo/chamada/RegisterChangeActivity.java
@@ -1,14 +1,24 @@
 package com.example.gustavo.chamada;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v4.media.MediaBrowserCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterChangeActivity extends Activity {
 
@@ -21,20 +31,70 @@ public class RegisterChangeActivity extends Activity {
     }
 
 
+
+
     public void changeNameClick(View view) {
+        /** Closure  Variables **/
+        final Context context = this;
         final EditText inputText = new EditText(this);
-        final String message = getString(R.string.password_textview);
+
+
+        /* Listener for succesful login (password confirmation) connection */
+        class LoginResponseListener implements Response.Listener<String> {
+            @Override
+            public void onResponse (String response) {
+                JSONObject obj = null;
+                try {
+                    obj = new JSONObject(response);
+                    if (obj.getString("success").equals ("true")) {
+                        /* Successful login*/
+                        inputText.setText("");
+                        ScreenUtils.showMessaDialog(context, "Password confirmed", null);
+                        inputText.setInputType(InputType.TYPE_CLASS_TEXT);
+
+                    }
+                    else {
+                        String s = context.getString(R.string.invalid_password);
+                        ScreenUtils.showMessaDialog(context, s, null);
+                    }
+                }
+                catch (Exception e) {
+                    String message = context.getString(R.string.no_server_connection);
+                    ScreenUtils.showMessaDialog(context, message, null);
+                    return;
+                }
+            }
+        }
+
+        /* Listener for login (password confirmation) error */
+        class LoginErrorListener implements Response.ErrorListener {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                String message = context.getString(R.string.no_server_connection);
+                ScreenUtils.showMessaDialog(context, message, null);
+            }
+        }
+
+        String message = getString(R.string.password_textview);
+        inputText.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
         ScreenUtils.showInputDialog(RegisterChangeActivity.this, message, inputText,
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        String name = inputText.getText().toString();
-                        Log.d(myActivityTag, "name = " + name);
+                        String password = inputText.getText().toString();
+                        ServerConnection sc = ServerConnection.getInstance(context);
+                        String userType = AppUser.getCurrentUser().getUserType();
+                        Map<String, String> params = new HashMap<String,String>();
+                        params.put("nusp", AppUser.getCurrentUser().getNusp());
+                        params.put("pass", password);
+                        sc.login(new LoginResponseListener(), new LoginErrorListener(),
+                                userType, params);
                     }
                 },
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        /*do nothing*/
                         return;
                     }});
     }
